9e9fd2305ff368f9d9171ec425121b412e81dea6
Niklas Helmertz <niklas.helmertz@gmail.com>
2014-12-15 16:58:24 +0100

    Add structure for JUnit tests

---
+++ papyrus-project/test/hotel/test/OrderingTest.java
@@ -0,0 +1,14 @@
+package hotel.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OrderingTest {
+
+	@Test
+	public void testOrdering() {
+		fail("Not yet implemented ");
+	}
+
+}
