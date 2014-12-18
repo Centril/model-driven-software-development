8015038b95e249762039c9a167ff10460529a1a8
Niclas Alexandersson <alniniclas@gmail.com>
2014-12-18 10:48:18 +0100

    Refactored SearchTest.

--- papyrus-project/test/hotel/test/SearchTest.java
+++ papyrus-project/test/hotel/test/SearchTest.java
@@ -1,25 +1,29 @@
 package hotel.test;
 
+import org.eclipse.emf.common.util.EList;
+import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.assertTrue;
 
 import Classes.Hotel.HotelFactory;
 import Classes.Hotel.ISearch;
+import Classes.Hotel.ISearchResult;
 
 public class SearchTest {
+	private ISearch search;
+
+	@Before
+	public void before() {
+		search = HotelFactory.eINSTANCE.createHotel_Hotel();
+	}
 
 	@Test(expected=IllegalArgumentException.class)
 	public void testZeroPeople() {
-		
-		ISearch search = HotelFactory.eINSTANCE.createHotel_Hotel();
-		
 		search.search(0, 1, 0);
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
 	public void testStartTimeAfterEndTime() {
-		
-		ISearch search = HotelFactory.eINSTANCE.createHotel_Hotel();
-		
 		search.search(1, 0, 1);
 	}
 
