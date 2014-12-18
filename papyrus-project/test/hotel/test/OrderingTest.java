c7379a8405c14a3e74e9d0261b398936de2c6b3e
Niclas Alexandersson <alniniclas@gmail.com>
2014-12-18 12:03:57 +0100

    Refactored OrderingTest.

--- papyrus-project/test/hotel/test/OrderingTest.java
+++ papyrus-project/test/hotel/test/OrderingTest.java
@@ -2,6 +2,8 @@ package hotel.test;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
+
 import javax.xml.soap.SOAPException;
 
 import org.junit.Before;
@@ -16,55 +18,70 @@ import Classes.Hotel.ISearch;
 import Classes.PersonRegistry.IPersonRegistry;
 
 public class OrderingTest {
-	
+	private static final CreditCardDetails TESLA = ccd("1212 1319 4143 1336", "666", 2, 2077, "Nikola", "Tesla", 100000);
+	private static final CreditCardDetails STALIN = ccd("1212 6666 6666 6666", "111", 3, 2856, "Joseph", "Stalin", 10000);
+	private static final CreditCardDetails DISNEY = ccd("1212 1919 1919 1919", "333", 4, 2356, "Walt", "Disney", 250000);
+
 	private CustomerRequires customerRequires;
 	private AdministratorRequires adminRequires;
-	private Hotel_Hotel hotel;
 	private ISearch iSearch;
 	private IOrdering iOrdering;
 	private IPersonRegistry iPersonReg;
 	
 	@Before
-	public void before() {
+	public void before() throws SOAPException {
 		// Ensure the existence of credit cards with money associated with some folks.
-		try {
-			customerRequires = CustomerRequires.instance();
-		} catch (SOAPException soppa) {
-			assertTrue(false);
-		}
-		try {
-			adminRequires = AdministratorRequires.instance();
-		} catch (SOAPException soppa) {
-			assertTrue(false);;
-		}
-		try {
-			if (!customerRequires.isCreditCardValid("1212 1319 4143 1336", "666", 2, 2077, "Nikola", "Tesla")) {
-				adminRequires.addCreditCard("1212 1319 4143 1336", "666", 2, 2077, "Nikola", "Tesla");
-			}
-			adminRequires.makeDeposit("1212 1319 4143 1336", "666", 2, 2077, "Nikola", "Tesla", 100000);
-			
-			if (!customerRequires.isCreditCardValid("1212 6666 6666 6666", "111", 3, 2856, "Joseph", "Stalin")) {
-				adminRequires.addCreditCard("1212 6666 6666 6666", "111", 3, 2856, "Joseph", "Stalin");
-			}
-			adminRequires.makeDeposit("1212 6666 6666 6666", "111", 3, 2856, "Joseph", "Stalin", 10000);
-			
-			if (!customerRequires.isCreditCardValid("1212 1919 1919 1919", "333", 4, 2356, "Walt", "Disney")) {
-				adminRequires.addCreditCard("1212 1919 1919 1919", "333", 4, 2356, "Walt", "Disney");
-			}
-			adminRequires.makeDeposit("1212 1919 1919 1919", "333", 4, 2356, "Walt", "Disney", 250000);
-			
-		} catch (SOAPException soppa) {
-			assertTrue(false);
+		customerRequires = CustomerRequires.instance();
+
+		adminRequires = AdministratorRequires.instance();
+
+		for (CreditCardDetails ccd : Arrays.asList(TESLA, STALIN, DISNEY)) {
+			setUpAccount(ccd);
 		}
 		
-		hotel = HotelFactory.eINSTANCE.createHotel_Hotel();
+		Hotel_Hotel hotel = HotelFactory.eINSTANCE.createHotel_Hotel();
 		iSearch = hotel;
 		iOrdering = hotel;
 		iPersonReg = hotel.getPersonRegistry();
 	}
-	
+
+	private void setUpAccount(CreditCardDetails ccd) throws SOAPException {
+		if (customerRequires.isCreditCardValid(ccd.ccNumber, ccd.ccv, ccd.expiryMonth, ccd.expiryYear, ccd.firstName, ccd.lastName)) {
+			adminRequires.removeCreditCard(ccd.ccNumber, ccd.ccv, ccd.expiryMonth, ccd.expiryYear, ccd.firstName, ccd.lastName);
+		}
+		adminRequires.addCreditCard(ccd.ccNumber, ccd.ccv, ccd.expiryMonth, ccd.expiryYear, ccd.firstName, ccd.lastName);
+		adminRequires.makeDeposit(ccd.ccNumber, ccd.ccv, ccd.expiryMonth, ccd.expiryYear, ccd.firstName, ccd.lastName, ccd.initialBalance);
+	}
+
 	@Test
 	public void testOrdering() {
 		
+	}
+
+	private static CreditCardDetails ccd (String ccNumber, String ccv, int expiryMonth,
+			int expiryYear, String firstName, String lastName, double initialBalance) {
+		return new CreditCardDetails(ccNumber, ccv, expiryMonth, expiryYear, firstName, lastName, initialBalance);
+	}
+
+	private static class CreditCardDetails {
+		final String ccNumber;
+		final String ccv;
+		final int expiryMonth;
+		final int expiryYear;
+		final String firstName;
+		final String lastName;
+		final double initialBalance;
+
+		public CreditCardDetails(String ccNumber, String ccv, int expiryMonth,
+				int expiryYear, String firstName, String lastName,
+				double initialBalance) {
+			this.ccNumber = ccNumber;
+			this.ccv = ccv;
+			this.expiryMonth = expiryMonth;
+			this.expiryYear = expiryYear;
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.initialBalance = initialBalance;
+		}
 	}
 }
