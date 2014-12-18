b56a27bca3d4c71ca53745e90c116715e837352f
Niclas Alexandersson <alniniclas@gmail.com>
2014-12-18 11:08:22 +0100

    Added tests for main flow in SearchTest.

--- papyrus-project/test/hotel/test/SearchTest.java
+++ papyrus-project/test/hotel/test/SearchTest.java
@@ -3,9 +3,10 @@ package hotel.test;
 import org.eclipse.emf.common.util.EList;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
 
+import static org.junit.Assert.assertTrue;
 import Classes.Hotel.HotelFactory;
+import Classes.Hotel.IBookingSuggestion;
 import Classes.Hotel.ISearch;
 import Classes.Hotel.ISearchResult;
 
@@ -33,4 +34,29 @@ public class SearchTest {
 		assertTrue(results.isEmpty());
 	}
 
+	@Test
+	public void testOneGuest() {
+		testNGuests(1);
+	}
+
+	@Test
+	public void testNGuests() {
+		for (int n = 2; n < 10; n++) {
+			testNGuests(n);
+		}
+	}
+	private void testNGuests(int n) {
+		EList<ISearchResult> results = search.search(1, 2, n);
+		for (ISearchResult result : results) {
+			assertEnoughBeds(n, result);
+		}
+	}
+
+	private static void assertEnoughBeds(int required, ISearchResult result) {
+		int beds = 0;
+		for (IBookingSuggestion bs: result.getBookingSuggestions()) {
+			beds += bs.getRoom().getNumBeds();
+		}
+		assertTrue("Too few beds.", beds >= required);
+	}
 }
