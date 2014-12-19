1311bcf62a678fe59160988ba74eb2c274635827
unknown <lucaspersson93@gmail.com>
2014-12-19 14:41:40 +0100

    Want to pull

--- papyrus-project/test/hotel/test/CheckOutTest.java
+++ papyrus-project/test/hotel/test/CheckOutTest.java
@@ -1,19 +1,31 @@
 package hotel.test;
 
 import static org.junit.Assert.*;
+import hotel.test.mock.MockBookingRequest;
+import hotel.test.mock.MockOrderRequest;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import Classes.Hotel.Hotel_Hotel;
 import Classes.Hotel.IFrontDesk;
+import Classes.Hotel.impl.Hotel_HotelImpl;
+import Classes.PersonRegistry.IPerson;
+import Classes.PersonRegistry.IPersonRegistry;
 
 public class CheckOutTest {
 	
 	private IFrontDesk frontdesk;
-
+	private Hotel_Hotel hotel;
+	private IPersonRegistry personRegistry;
+	private IPerson person;
+	
 	@Before
 	public void before() {
-		frontdesk = IFrontDesk.instance;
+		hotel = Hotel_HotelImpl.getInstance();
+		frontdesk = hotel;
+		personRegistry = IPersonRegistry.instance;
+		person = personRegistry.createPerson(0);
 	}
 	@Test 
 	public void testCheckOutwithNoBooking() {
