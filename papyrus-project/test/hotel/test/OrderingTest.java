072f37df1cab8156dd0f699e87324c6f98bd96f1
Niklas Helmertz <niklas.helmertz@gmail.com>
2014-12-19 15:54:39 +0100

    Additional test for ordering, assert data correct

--- papyrus-project/test/hotel/test/OrderingTest.java
+++ papyrus-project/test/hotel/test/OrderingTest.java
@@ -20,7 +20,10 @@ import se.chalmers.cse.mdsd1415.banking.customerRequires.CustomerRequires;
 import Classes.Hotel.BookingRequest;
 import Classes.Hotel.HotelFactory;
 import Classes.Hotel.Hotel_Hotel;
+import Classes.Hotel.IBooking;
 import Classes.Hotel.IConfiguration;
+import Classes.Hotel.IFrontDesk;
+import Classes.Hotel.IOrder;
 import Classes.Hotel.IOrdering;
 import Classes.Hotel.ISearch;
 import Classes.Hotel.ISearchResult;
@@ -33,6 +36,7 @@ public class OrderingTest {
 	private static final CreditCardDetails TESLA = ccd("1212131941431336", "666", 2, 16, "Nikola", "Tesla", 100000);
 	private static final CreditCardDetails STALIN = ccd("1212666666666666", "111", 3, 16, "Joseph", "Stalin", 10000);
 	private static final CreditCardDetails DISNEY = ccd("1212191919191919", "333", 4, 16, "Walt", "Disney", 250000);
+	private static final long MILLIS_IN_DAY = 86400000L;
 
 	private CustomerRequires customerRequires;
 	private AdministratorRequires adminRequires;
@@ -40,6 +44,7 @@ public class OrderingTest {
 	private IOrdering iOrdering;
 	private IPersonRegistry iPersonReg;
 	private IConfiguration iConfiguration;
+	private IFrontDesk iFrontDesk;
 	
 	@Before
 	public void before() throws SOAPException {
@@ -56,7 +61,8 @@ public class OrderingTest {
 		iSearch = hotel;
 		iOrdering = hotel;
 		iConfiguration = hotel;
-		iPersonReg = PersonRegistryFactory.eINSTANCE.createPersonRegistry_PersonRegistry();
+		iFrontDesk = hotel;
+		iPersonReg = hotel.getPersonRegistry(); // If not for tests, IPersonRegistry.instance would be used
 	}
 
 	private void setUpAccount(CreditCardDetails ccd) throws SOAPException {
@@ -118,6 +124,35 @@ public class OrderingTest {
 		
 		assertTrue(ordering.placeOrder(orderRequest));
 	}
+	
+	@Test
+	public void testSimplestPossibleOrdering() {
+		iConfiguration.createRoom(1, 1);
+		
+		IPerson person = iPersonReg.createPerson(0);
+		person.createCreditCard(TESLA.ccNumber, TESLA.ccv, TESLA.expiryMonth, TESLA.expiryYear, TESLA.firstName, TESLA.lastName);
+		int personID = person.getId();
+
+		long now = System.currentTimeMillis();
+		List<ISearchResult> searchResults = iSearch.search(now + MILLIS_IN_DAY, now + 2 * MILLIS_IN_DAY, 1);
+		// Assumes that search is working
+
+		BookingRequest br = new MockBookingRequest(searchResults.get(0).getBookingSuggestions().get(0), Arrays.asList(personID), personID);
+		OrderRequest or = new MockOrderRequest(personID, Arrays.asList(br));
+
+		assertTrue(iOrdering.placeOrder(or));
+
+		List<IOrder> orders = iFrontDesk.getOrders();
+		assertEquals(1, orders.size());
+		IOrder order = orders.get(0);
+		assertEquals(personID, order.getCustomer());
+		assertEquals(1, order.getBookings().size());
+		IBooking booking = order.getBookings().get(0);
+		assertEquals(personID, booking.getContact());
+		assertEquals(1, booking.getGuests().size());
+		assertEquals(personID, booking.getGuests().get(0).intValue());
+	}
+
 
 	private static CreditCardDetails ccd (String ccNumber, String ccv, int expiryMonth,
 			int expiryYear, String firstName, String lastName, double initialBalance) {
