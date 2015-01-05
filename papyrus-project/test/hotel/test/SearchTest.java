436603f410ed4e0899566f5e85388754964f142a
Niklas Helmertz <niklas.helmertz@gmail.com>
2015-01-05 16:24:25 +0100

    One too many removed...

--- papyrus-project/test/hotel/test/SearchTest.java
+++ papyrus-project/test/hotel/test/SearchTest.java
@@ -10,6 +10,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import Classes.Hotel.HotelFactory;
+import Classes.Hotel.Hotel_Hotel;
 import Classes.Hotel.IBookingSuggestion;
 import Classes.Hotel.IConfiguration;
 import Classes.Hotel.ISearch;
